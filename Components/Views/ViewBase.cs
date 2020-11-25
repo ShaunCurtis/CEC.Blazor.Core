a4cca67004e1fa5ded6856bc615c20e801901c05
Shaun Curtis <shaun@coldelm.com>
2020-11-25 14:38:08 +0000

    Added ViewBase

---
+++ Components/Views/ViewBase.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+
+namespace CEC.Blazor.Core
+{
+    public class ViewBase : Component, IView
+    {
+        [CascadingParameter]
+        public ViewManager ViewManager { get; set; }
+    }
+}
